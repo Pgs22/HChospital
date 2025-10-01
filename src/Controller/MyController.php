ff80f457dcd90271443233d97560088ca226fda6
patricia <patricia.gsbcn@gmail.com>
2025-10-01 16:15:50 +0200

    completeLogin

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -7,26 +7,53 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 //Para identificar que todas las funciones estan en la misma carpeta
-#[Route('/nurse')]
+//#[Route('/nurse')]
 final class MyController extends AbstractController
 {
     //JsonResponse formato estandard para todos los ficheros
-    #[Route('/nurse/login', name: 'app_Nurse')]
-    public function login(string $name, string $password): JsonResponse
+    #[Route('/nurse/login/', name: 'app_Nurse')]
+    //public function login(string $name, string $password): JsonResponse
+    public function login(Request $request): JsonResponse
     {
-        // get parameter input(user, pw)
 
-        
+        //ejemplo: http://localhost:8080/nurse/login?nombreparametro=valor&nombreparametro2=valor2
+        // get parameter input(user, password)
+        $user = $request->query->get('user');
+        $password = $request->query->get('password');                
             //Indicamos ruta del json
         $jsonFilePath = $this->getParameter('kernel.project_dir') . '/public/data/nurses.json';
 
-
         // compare input con nurses.json
-
-        //Para leer el contenido del archivo
+            //Para leer el contenido del archivo
         $jsonContent = file_get_contents($jsonFilePath);
-        //Para añadir los objetos json a un array asociativo
+            //Para añadir los objetos json a un array asociativo
         $users = json_decode($jsonContent, true);
+
+        // Para buscar y comparar datos en la url con datos del array extraídos del json
+        foreach ($users as $user) {
+            if ($user['user'] === $user && 
+                $password['password'] === $password) {
+                
+                $loginSuccess = true;
+                break;
+            }
+        }
+        
+        // Retorna true si coinciden y false sino
+        if ($loginSuccess) {
+            return new JsonResponse(
+                ['success' => true, 'message' => 'True'], 
+                status: Response::HTTP_OK // Código 200 OK
+            );
+        } else {
+            // Error 401 Unauthorized o 400 Bad Request
+            return new JsonResponse(
+                ['success' => false, 'message' => 'False'], 
+                status: Response::HTTP_UNAUTHORIZED 
+            ); 
+        }
+        
+        /*
         //Busca valor name
         $nameExists = false;
         foreach ($users as $user) {
@@ -35,14 +62,9 @@ final class MyController extends AbstractController
                 break;
             }
         }
-
         // si son iguales ret true , si no false
         return new JsonResponse(['exists' => $nameExists], status: Response::HTTP_OK);
-        
-        /*
-        return $this->json([
-            'message' => 'true'
-        ]);
         */
+        
     }
 }
