f11495410ec0278af38caea42c96743d6c266be6
patricia <patricia.gsbcn@gmail.com>
2025-10-02 15:55:26 +0200

    2ejemplos

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse; //Para salida de datos de un json
+use Symfony\Component\HttpFoundation\Response; //Para salida de datos de un boolean
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request; //Para la entrada de datos
+//use Symfony\Component\HttpFoundation\JsonRequest; //Para entrada de datos de un json si vienen de fuera
+
 //Para identificar que todas las funciones estan en la misma carpeta
 //#[Route('/nurse')]
 final class MyController extends AbstractController
@@ -16,29 +19,38 @@ final class MyController extends AbstractController
     public function login(Request $request): JsonResponse
     {
 
+        //************************************************************************************************
+        //RECUPERAR PARAMETROS
         //ejemplo: http://localhost:8080/nurse/login?nombreparametro=valor&nombreparametro2=valor2
         // get parameter input(user, password)
         $user = $request->query->get('user');
         $password = $request->query->get('password');                
-            //Indicamos ruta del json
-        $jsonFilePath = $this->getParameter('kernel.project_dir') . '/public/data/nurses.json';
 
+
+        //************************************************************************************************
+        //RECUPERAR FICHERO JSON
+        //Indicamos ruta del json
+        $jsonFilePath = $this->getParameter('kernel.project_dir') . '/public/data/nurses.json';
         // compare input con nurses.json
             //Para leer el contenido del archivo
         $jsonContent = file_get_contents($jsonFilePath);
             //Para añadir los objetos json a un array asociativo
-        $users = json_decode($jsonContent, true);
+        $usersData = json_decode($jsonContent, true);
 
+        //************************************************************************************************
+        //PARA BUSCAR Y COMPARAR        
         // Para buscar y comparar datos en la url con datos del array extraídos del json
-        foreach ($users as $user) {
-            if ($user['user'] === $user && 
-                $password['password'] === $password) {
+        foreach ($usersData as $userData) {
+            if ($userData['user'] === $user && 
+                $userData['password'] === $password) {
                 
                 $loginSuccess = true;
                 break;
             }
         }
-        
+
+        //************************************************************************************************
+        //DEVUELVE RESULTADO        
         // Retorna true si coinciden y false sino
         if ($loginSuccess) {
             return new JsonResponse(
@@ -64,6 +76,7 @@ final class MyController extends AbstractController
         }
         // si son iguales ret true , si no false
         return new JsonResponse(['exists' => $nameExists], status: Response::HTTP_OK);
+        //return $nameExists; //Si quiero que me devuelva un boolean en vez de un json
         */
         
     }
